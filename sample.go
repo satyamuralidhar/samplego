c82f6f9fa18360503b24867b6d1a910d1a4f181f
satya <38804803+satyamuralidhar@users.noreply.github.com>
2020-02-06 14:21:28 +0530

    Create sample.go

---
+++ sample.go
@@ -0,0 +1,21 @@
+package main 
+
+import (
+
+	"fmt"
+
+	"math"
+
+)
+
+func main() {
+
+	var num float64 = 12
+
+	var result = math.Sqrt(num)
+
+	fmt.Println(result)
+
+	fmt.Printf("the values is %.2f", result)
+
+}
